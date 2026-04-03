2364d4b19b838db79a330f76f9e048e07a15a8e4
Kazan <kazandjiannicolas@gmail.com>
2026-04-03 13:46:00 -0300

    cambios a la lógica del listas

--- src/list/ListExercise.java
+++ src/list/ListExercise.java
@@ -3,6 +3,7 @@ package list;
 import application.Exercise;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ListExercise extends Exercise {
@@ -20,40 +21,52 @@ public class ListExercise extends Exercise {
     protected void exerciseLogic() {
         switch (currentPhase){
             case 0:
-                System.out.println(list.stream().toList());
-                System.out.println("Bienvenido " +
-                        "\n su lista actual es: " + list.toString() +
-                        "\n A- Agregar elemento a la lista" +
-                        "\n B- remover elemento por índice" +
-                        "\n C- Remover por referencia" +
-                        "\n D- clear");
+                menuLogic();
                 break;
             case 1:
-
-
-
+                addElement();
+                break;
+            case 2:
+                removeElementByIndex();
+                break;
+            case 3:
+                removeElementByReference();
+                break;
+            case 4:
+                clear();
+                break;
         }
-        menuLogic();
-        scanner.close();
     }
 
 
     private void menuLogic(){
+        System.out.println("Bienvenido " +
+                "\n su lista actual es: " + list.toString() +
+                "\n A- Agregar elemento a la lista" +
+                "\n B- remover elemento por índice" +
+                "\n C- Remover por referencia" +
+                "\n D- clear");
         String userInput = scanner.nextLine().toUpperCase();
         switch (userInput) {
             case "A":
-                addElement();
+                currentPhase = 1;
+                exerciseLogic();
                 break;
             case "B":
-                removeElementByIndex();
+                currentPhase = 2;
+                exerciseLogic();
                 break;
             case "C":
-                removeElementByReference();
+                currentPhase = 3;
+                exerciseLogic();
                 break;
             case "D":
-                clear();
+                currentPhase = 4;
+                exerciseLogic();
                 break;
-
+            default:
+                System.out.println("ingrese otro input");
+                menuLogic();
         }
     }
 
@@ -61,12 +74,32 @@ public class ListExercise extends Exercise {
         System.out.println("Cargue nuevo elemento");
         String newValue = scanner.nextLine().toUpperCase();
         list.add(newValue);
+        System.out.println(list);
+        System.out.println("Desea agregar otro elemento? y/otro valor");
+        String userInput = scanner.nextLine();
+        if(Objects.equals(userInput, "y")){
+            addElement();
+        } else {
+            currentPhase = 0;
+            menuLogic();
+        }
     }
 
     private void removeElementByIndex() {
+        System.out.println("Su lista actual es: " + list);
         System.out.println("Indique el índice de referencia");
-        String index = scanner.nextLine().toUpperCase();
+        int index = scanner.nextInt();
         list.remove(index);
+        System.out.println(list);
+        System.out.println("Desea eliminar otro elemento? y/otro valor");
+        String userInput = scanner.nextLine();
+        if(Objects.equals(userInput, "y")){
+            removeElementByIndex();
+        } else {
+            currentPhase = 0;
+            menuLogic();
+        }
+
     }
     private void removeElementByReference() {
         System.out.println("Indique el elemento de referencia");
